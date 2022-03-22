69bef75f8da8c86488745fddba8334f5d24f4104
latruonghai <latruonghai@gmail.com>
2022-03-22 09:19:11 +0700

    Delete LoginForm component

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,24 +1,15 @@
 import React, { Fragment } from "react";
 import Form from "../components/Forms";
 import { LoginState } from "../types";
-const LoginPage = (props: LoginState) =>{
+const LoginPage = (props: LoginState) => {
     return (
         <div>
             {
-                props.isLogin && 
+                props.isLogin &&
                 <Form type="Login"></Form>
             }
         </div>
     )
 }
 
-
-const LoginForm = (props:any) =>{
-    
-    return (
-        <Fragment>
-            
-        </Fragment>
-    )
-}
 export default LoginPage;
